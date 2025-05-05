ed86419de66f5ef9ebc263277ef6c60b3e9d0158
Ore Adeleye <oreadeleye67@gmail.com>
2025-05-06 00:20:05 +0100

    Refactor Play scene to prevent duplicate animation creation and enhance player controls with additional key bindings for A, W, and D keys.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -43,27 +43,39 @@ export class Play extends Phaser.Scene {
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
 
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
-    });
+    if (!this.anims.get('left')) {
+      this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+      });
+    }
 
-    this.anims.create({
-      key: 'turn',
-      frames: [{ key: 'dude', frame: 4 }],
-      frameRate: 20
-    });
+    if (!this.anims.get('turn')) {
+      this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+      });
+    }
 
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
-    });
+    if (!this.anims.get('right')) {
+      this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+      });
+    }
 
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys({
+      a: Phaser.Input.Keyboard.KeyCodes.A,
+      w: Phaser.Input.Keyboard.KeyCodes.W,
+      d: Phaser.Input.Keyboard.KeyCodes.D
+    });
+
 
     this.stars = this.physics.add.group({
       key: 'star',
@@ -100,16 +112,21 @@ export class Play extends Phaser.Scene {
   update() {
     if (this.gameOver) {
       if (this.cursors.space.isDown) {
+        this.physics.resume();
+        this.gameOver = false;
+        this.gameOverText.setVisible(false);
+        this.score = 0;
+        this.scoreText.setText('Score: 0');
         this.scene.restart();
       } else {
         return;
       }
     }
 
-    if (this.cursors.left.isDown) {
+    if (this.cursors.left.isDown || this.keys.a.isDown) {
       this.player.setVelocityX(-160);
       this.player.anims.play('left', true);
-    } else if (this.cursors.right.isDown) {
+    } else if (this.cursors.right.isDown || this.keys.d.isDown) {
       this.player.setVelocityX(160);
       this.player.anims.play('right', true);
     } else {
@@ -117,7 +134,7 @@ export class Play extends Phaser.Scene {
       this.player.anims.play('turn');
     }
 
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
+    if ((this.keys.w.isDown || this.cursors.up.isDown) && this.player.body.touching.down) {
       this.player.setVelocityY(-330);
     }
   }
